3c6acce32e4d47a50e5e0225cb1401d342519cb1
o-henry-coder <olyalya112c.91@gmail.com>
2020-12-14 22:31:15 +0200

    lesson01

---
+++ univer/lesson01/task01.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.showturtle()
+# turtle.forward(200)
+# turtle.right(90)
+# turtle.forward(200)
+turtle.forward(50)
+turtle.setheading(90)
+turtle.forward(100)
+turtle.setheading(180)
+turtle.forward(50)
+turtle.setheading(90)
+turtle.forward(100)
