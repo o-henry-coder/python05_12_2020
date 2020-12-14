e51772535d416c9b8bb95e662ef93e900ce5e02f
o-henry-coder <75783011+o-henry-coder@users.noreply.github.com>
2020-12-14 22:39:52 +0200

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,11 @@
+date = int(input("please enter the month number "))
+if date == 1 or date == 2 or date == 12:
+    print("this is winter")
+if date == 3 or date == 4 or date == 5:
+    print("this is spring")
+if date == 6 or date == 7 or date == 8:
+    print("this is summer")
+if date == 9 or date == 10 or date == 11:
+    print("this is autumn")
+
+
